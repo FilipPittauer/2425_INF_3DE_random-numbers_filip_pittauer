1a7115800a97c1671c8f269931c40d65ff72291a
3.D Pittauer Filip <Filip.Pittauer@student.sps-cl.cz>
2024-10-07 12:05:28 +0200

    sequence dokoncena

--- sequence.py
+++ sequence.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_sequence(n):
-    return random.randint(1, 10)
-
+    return [random.randint(1, n) for _ in range(n)]
+    
 # Otestování funkce
-generate_sequence(10)  # Vygeneruje 10 náhodných čísel
+print(generate_sequence(10))  # Vygeneruje 10 náhodných čísel
