3e0ff74a2207eb241d5f4bb783b6f38ec3a7af60
3.D Pittauer Filip <Filip.Pittauer@student.sps-cl.cz>
2024-10-07 11:55:24 +0200

    multiple_dice dokoncena

--- multiple_dice.py
+++ multiple_dice.py
@@ -1,8 +1,7 @@
 import random
 
 def roll_multiple_dice(n, k):
-    # Tvůj kód zde
-    pass
-
+    return [random.randint(1, k) for _ in range(n)]
+    
 # Otestování funkce
 print(roll_multiple_dice(3, 6))  # Simulace hodu 3 kostkami, každá má 6 hran
--- sequence.py
+++ sequence.py
@@ -1,8 +1,7 @@
 import random
 
 def generate_sequence(n):
-    # Tvůj kód zde
-    pass
+    return random.randint(1, 10)
 
 # Otestování funkce
 generate_sequence(10)  # Vygeneruje 10 náhodných čísel
